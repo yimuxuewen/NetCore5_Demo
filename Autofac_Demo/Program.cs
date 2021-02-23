7dad68b94bff79683a6d3f3077dea2463a9817f8
yimuxuewen <yimuxuewen@sina.com>
2021-02-23 16:12:09 +0800

    初始化

--- Autofac_Demo/Program.cs
+++ Autofac_Demo/Program.cs
@@ -14,7 +14,7 @@ namespace Autofac_Demo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); ;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
